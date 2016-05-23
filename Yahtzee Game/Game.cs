9d1adef5ba37badc2853507fa8c604b8a7395ca8
lch19931009 <lch19931009@hotmail.com>
2016-05-23 14:02:55 +1000

    Fixing Git

--- Yahtzee Game/Game.cs
+++ Yahtzee Game/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Yahtzee_Game {
 
@@ -15,8 +16,48 @@ namespace Yahtzee_Game {
     }
 
     class Game {
+        private Player[] players;
+        private int currentPlayerIndex;
+        private Player currentPlayer;
+        private Die[] Dice;
+        private int playersFinished;
+        private int numRolls;
+        private Form1 form;
+        private Label[] dieLabels;
 
-        
+        public Game(Form1) {
+            Player[] players = new Player[2];
+            currentPlayerIndex = 1;
+            currentPlayer = 1;
+            //Dice
+        }
 
+        public void NextTurn() {
+
+        }
+
+        public void RollDice() {
+
+        }
+
+        public int HoldDie() {
+
+        }
+
+        public int ReleaseDie() {
+
+        }
+
+        public ScoreType ScoreCombination() {
+
+        }
+
+        public Game static Load(Form1) {
+
+        }
+
+        public Save() {
+
+        }
     }
 }
