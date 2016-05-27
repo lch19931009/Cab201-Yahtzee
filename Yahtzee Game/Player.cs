88d6ec1d8e988af88882e03cb0b7227cdfceed5b
lch19931009 <lch19931009@hotmail.com>
2016-05-27 14:03:08 +1000

    player updates

--- Yahtzee Game/Player.cs
+++ Yahtzee Game/Player.cs
@@ -12,35 +12,19 @@ namespace Yahtzee_Game {
         private Score[] scores = new Score[(int)ScoreType.GrandTotal + 1];
         private int grandTotal;
 
-        public Player(string name, Label[] labels) {
+        public Player(string name) {
             Label[] scoreTotals = new Label[(int)ScoreType.GrandTotal + 1];
             this.name = name;
         }
 
-        public string Name {get; protected set;}
+        public string Name {
+            get {
+                return name;
+            }
+        }
 
         public void ScoreCombination(ScoreType score, int[] dice) {
         }
 
-        public int GrandTotal { get; protected set; }
-
-        public bool IsAvailable(ScoreType) {
-            return true;
-        }
-
-        public void ShowScores() {
-        }
-
-        public bool IsFinished() {
-            /*if () {
-                return true;
-            }
-            */
-            return false;
-        }
-
-        public void Load(Label[] labels) {
-
-        }
     }
 }
