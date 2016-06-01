13503fc21049bfaa16bab7eb6015f23c3d715bab
scorpioncoder <alexjbarnier@gmail.com>
2016-06-01 15:14:51 +1000

    More on Part D

---
+++ Yahtzee Game/CountingCombination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahtzee_Game {
+    class CountingCombination : Combination {
+
+        private int dieValue;
+
+        public CountingCombination(Label combinationLabel) : base(combinationLabel) {
+            dieValue = (int)(ScoreType)combinationLabel.Tag;
+        }
+        
+        public override void CalculateScore(int[] values) {
+            foreach(int value in values) {
+                if(value == dieValue) {
+                    Points++;
+                }
+            }
+        }
+    }
+}
